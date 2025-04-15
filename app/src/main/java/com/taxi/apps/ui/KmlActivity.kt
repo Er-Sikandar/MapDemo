cac727223159bb4b533fd010d10c584c53962530
Az-Sikandar <sikandar@appsquadz.com>
2025-04-15 10:55:16 +0530

    Kml Files

--- app/src/main/java/com/taxi/apps/ui/KmlActivity.kt
+++ app/src/main/java/com/taxi/apps/ui/KmlActivity.kt
@@ -8,10 +8,14 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import com.google.maps.android.data.kml.KmlContainer
 import com.google.maps.android.data.kml.KmlLayer
+import com.google.maps.android.data.kml.KmlLineString
+import com.google.maps.android.data.kml.KmlPoint
 import com.google.maps.android.data.kml.KmlPolygon
-import com.google.maps.android.data.kml.KmlStyle
 import com.taxi.apps.R
 import com.taxi.apps.databinding.ActivityKmlBinding
 import java.net.HttpURLConnection
@@ -31,6 +35,8 @@ class KmlActivity : AppCompatActivity(), OnMapReadyCallback{
         if (mapFragment != null) {
             mapFragment.getMapAsync(this)
         }
+
+
         binding.btnCheckIn.setOnClickListener{
 
         }
@@ -43,45 +49,74 @@ class KmlActivity : AppCompatActivity(), OnMapReadyCallback{
         try {
             val kmlLayer = KmlLayer(mMap, R.raw.suitability_map, getApplicationContext())
             kmlLayer.addLayerToMap()
-            for (container in kmlLayer.containers) {
-                getPlacemarksFromContainer(container)
+            kmlLayer.setOnFeatureClickListener { feature ->
+                Log.e("KML_CLICK", "Clicked feature with ID: ${feature.id}")
+                val geometry = feature.geometry
+                if (geometry is KmlPolygon) {
+                    Log.e("KML", "Polygon Coordinates:")
+                    val outerBoundary = geometry.outerBoundaryCoordinates
+                    for (latLng in outerBoundary) {
+                        Log.e("KML", "Lat: ${latLng.latitude}, Lng: ${latLng.longitude}")
+                    }
+                    val center = getPolygonCenter(outerBoundary)
+                    mMap?.addMarker(MarkerOptions()
+                            .position(center)
+                            .title("Polygon Clicked")
+                            .snippet("Feature ID: ${feature.id}"))
+                }
             }
+           /* for (container in kmlLayer.containers) {
+                getPlacemarksFromContainer(container)
+            }*/
+
         } catch (e: Exception) {
             Log.e(TAG, "onMapReady Ex: ${e.message}")
             e.printStackTrace()
         }
-       /* val kmlUrl = "https://developers.google.com/maps/documentation/javascript/examples/kml/westcampus.kml"
-       // val kmlUrl = "http://developers.google.com/kml/documentation/KML_Samples.kml"
-        Thread {
-            try {
-                val url = URL(kmlUrl)
-                val connection = url.openConnection() as HttpURLConnection
-                connection.connect()
-                if (connection.responseCode == HttpURLConnection.HTTP_OK) {
-                    val inputStream = connection.inputStream
-                    // Create KmlLayer from InputStream
-                    val kmlLayer = KmlLayer(mMap, inputStream, applicationContext)
-                    runOnUiThread {
-                        kmlLayer.addLayerToMap()
-                    }
-                } else {
-                    Log.e(TAG, "HTTP error: ${connection.responseCode}")
-                }
-
-            } catch (e: Exception) {
-                Log.e(TAG, "KML load error: ${e.message}")
-                e.printStackTrace()
-            }
-        }.start()*/
 
 
     }
+    fun getPolygonCenter(points: List<LatLng>): LatLng {
+        var lat = 0.0
+        var lng = 0.0
+        for (point in points) {
+            lat += point.latitude
+            lng += point.longitude
+        }
+        val size = points.size
+        return LatLng(lat / size, lng / size)
+    }
+
 
     private fun getPlacemarksFromContainer(container: KmlContainer) {
         for (placemark in container.placemarks) {
             val styleUrl = placemark.styleId // e.g., "#high"
-
             val geometry = placemark.geometry
+
+            val name = placemark.getProperty("name") ?: "Unknown"
+            when (geometry) {
+                is KmlPolygon -> {
+                    val polygon = mMap?.addPolygon(PolygonOptions()
+                            .addAll(geometry.outerBoundaryCoordinates)
+                            .clickable(true))
+                    polygon?.tag = name
+                }
+                is KmlPoint -> {
+                    val position = LatLng(geometry.geometryObject.latitude, geometry.geometryObject.longitude)
+                    val marker = mMap?.addMarker(MarkerOptions()
+                            .position(position).title(name)
+                    )
+                    marker?.tag = name
+                }
+                is KmlLineString -> {
+                    val polyline = mMap?.addPolyline(PolylineOptions()
+                            .addAll(geometry.geometryObject)
+                            .color(0xFF0000FF.toInt()).clickable(true)
+                    )
+                    polyline?.tag = name
+                }
+            }
+
             if (geometry is KmlPolygon) {
                 Log.e("KML", "Style: $styleUrl")
                 Log.e("KML", "Polygon Coordinates:")
